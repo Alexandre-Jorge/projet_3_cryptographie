11a1fdfd4de93b0fb83cb1864de5c48fa21dad42
Alexandre-Jorge <alexj77380@gmail.com>
2023-12-06 12:37:50 +0100

    add css

--- index.php
+++ index.php
@@ -23,10 +23,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Our brand</h1>
+    <h1>Jack Sparrhum</h1>
     <h2>Verify prodcut autenticity</h2>
     <form action="index.php" method="post">
         <input type="text" name="uid" placeholder="11:22:aa:bb">
